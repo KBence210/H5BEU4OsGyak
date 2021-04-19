a1738a44ad79c8dcf4dc6d8b111f425147b406e5
KBence210 <79148226+KBence210@users.noreply.github.com>
2021-04-19 14:13:32 +0200

    Add files via upload

--- 04.06/h5beu4_alarmra_var.c
+++ 04.06/h5beu4_alarmra_var.c
@@ -1,1 +1,18 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void do_nothing();
+
+
+main ()
+{
+
+
+	signal(SIGALRM, do_nothing);
+	printf(" varok de meddig?\n");
+	pause();
+    	printf("  Vegre, itt az alarm \n");
+}	
+void do_nothing(){ ;}
