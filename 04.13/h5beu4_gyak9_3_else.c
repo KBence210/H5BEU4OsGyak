4a776b535efdd01ba45fbae799b11c25aa3306a9
KBence210 <79148226+KBence210@users.noreply.github.com>
2021-04-26 18:22:41 +0200

    Add files via upload
    Hibás számítógép miatti lemaradás pótlása.

---
+++ 04.13/h5beu4_gyak9_3_else.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <signal.h>
+
+void handleSigalarm();
+
+void main() {
+    printf("A program pidje : %d\n",getpid());
+    signal(SIGALRM, handleSigalarm);
+    pause();
+    printf("kibillent\n");
+    exit(0);
+}
+
+void handleSigalarm() {
+    printf("h5bau4\n");
+    
--- 04.13/h5beu4_named.c
+++ 04.13/h5beu4_named.c
@@ -1,1 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+#define FN "jmdrgg"
+
+void main() {
+    int nbytes;
+    pid_t childpid;
+    char string[] = "Nagy Robert\n";
+    char readbuffer[80];
+    
+    mkfifo(FN, S_IFIFO | 0666);    
+
+    if ((childpid = fork()) == -1) {
+            perror("fork hiba");
+            exit(1);
+    }
+
+    if (childpid == 0) {
+        int fd = open(FN, O_WRONLY);
+        write(fd, string, sizeof(string));
+        close(fd);
+        unlink(FN);
+    } else {      
+        int fd2 = open(FN, O_RDONLY);
+        nbytes = read(fd2, &readbuffer, sizeof(readbuffer));
+        close(fd2);
+        unlink(FN);
+        printf("%s", readbuffer);
+    }  
+}
